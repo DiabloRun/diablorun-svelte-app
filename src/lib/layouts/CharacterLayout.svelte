d9a2b837b5fa11b4ff9a7ec61a88a9d440379042
Indrek <leede.indrek@gmail.com>
2023-09-05 18:17:02 +0300

    Initial layout

---
+++ src/lib/layouts/CharacterLayout.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import { AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
+
+	let currentTile: number = 0;
+</script>
+
+<AppShell
+	slotPageContent="p-12"
+	slotHeader="border-b border-surface-700"
+	slotFooter="bg-surface-800 p-4 border-t border-surface-700"
+	slotSidebarLeft="border-r border-surface-700"
+	slotSidebarRight="p-4 bg-surface-800 w-[360px] border-l border-surface-700"
+>
+	<svelte:fragment slot="header">
+		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+			<svelte:fragment slot="lead">
+				<img src="/logo.png" alt="Diablo.run" class="h-7" />
+			</svelte:fragment>
+			<h1><slot name="title" /></h1>
+			<svelte:fragment slot="trail">
+				<button type="button" class="variant-filled btn-icon">(icon)</button>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<AppRail>
+			<svelte:fragment slot="lead">
+				<AppRailAnchor href="/">(icon)</AppRailAnchor>
+			</svelte:fragment>
+			<!-- --- -->
+			<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="Items">
+				<svelte:fragment slot="lead">(icon)</svelte:fragment>
+				<span>Items</span>
+			</AppRailTile>
+			<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="Inventory">
+				<svelte:fragment slot="lead">(icon)</svelte:fragment>
+				<span>Inventory</span>
+			</AppRailTile>
+			<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="Skills">
+				<svelte:fragment slot="lead">(icon)</svelte:fragment>
+				<span>Stash</span>
+			</AppRailTile>
+			<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="Skills">
+				<svelte:fragment slot="lead">(icon)</svelte:fragment>
+				<span>Cube</span>
+			</AppRailTile>
+			<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="Skills">
+				<svelte:fragment slot="lead">(icon)</svelte:fragment>
+				<span>Merc</span>
+			</AppRailTile>
+			<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="Skills">
+				<svelte:fragment slot="lead">(icon)</svelte:fragment>
+				<span>Skills</span>
+			</AppRailTile>
+			<!-- --- -->
+			<svelte:fragment slot="trail">
+				<AppRailAnchor href="/" target="_blank" title="Account">(icon)</AppRailAnchor>
+			</svelte:fragment>
+		</AppRail>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarRight">
+		<slot name="sidebarLeft" />
+	</svelte:fragment>
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<!-- (pageFooter) -->
+	<svelte:fragment slot="footer">
+		<slot name="footer" />
+	</svelte:fragment>
+</AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,161 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import CharacterLayout from '$lib/layouts/CharacterLayout.svelte';
+	import { onMount } from 'svelte';
+	import { popup } from '@skeletonlabs/skeleton';
+	import type { PopupSettings } from '@skeletonlabs/skeleton';
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
-</div>
+	const popupHover: PopupSettings = {
+		event: 'hover',
+		target: 'popupHover',
+		placement: 'bottom'
+	};
+
+	let character: any = {
+		attributes: {
+			strength: 0,
+			dexterity: 0,
+			vitality: 0,
+			energy: 0
+		},
+		resitances: {
+			fire: 0,
+			cold: 0,
+			lightning: 0,
+			poison: 0
+		}
+	};
+	let items: any[] = [];
+	let quests: any[] = [];
+	let error: string | null = null;
+	let isLoading = true;
+
+	onMount(async () => {
+		try {
+			const response = await fetch('https://api.diablo.run/snapshots/users/RyuQuezacotl');
+
+			if (!response.ok) {
+				throw new Error(`HTTP error: ${response.status} - ${response.statusText}`);
+			}
+
+			const responseData = await response.json();
+			character = {
+				...responseData.character,
+				attributes: {
+					strength: responseData.character.strength,
+					dexterity: responseData.character.dexterity,
+					vitality: responseData.character.vitality,
+					energy: responseData.character.energy
+				},
+				resistances: {
+					fire: responseData.character.fire_res,
+					cold: responseData.character.cold_res,
+					lightning: responseData.character.light_res,
+					poison: responseData.character.poison_res
+				}
+			};
+			items = responseData.items;
+			quests = responseData.quests;
+		} catch (err) {
+			console.error('Error:', err);
+			error = 'An error occurred while fetching data.';
+		} finally {
+			isLoading = false;
+		}
+	});
+</script>
+
+{#if isLoading}
+	<p>Loading data...</p>
+{:else if error}
+	<p>{error}</p>
+{:else}
+	<CharacterLayout>
+		<svelte:fragment slot="title">
+			<ol class="breadcrumb">
+				<li class="crumb">
+					<a href="/elements/breadcrumbs" class="variant-soft-primary btn btn-sm">Users</a>
+				</li>
+				<li class="crumb-separator" aria-hidden>&rsaquo;</li>
+				<li class="crumb">
+					<a href="/">
+						<img
+							src={character.user_profile_image_url}
+							alt={character.user_name}
+							class="h-10 w-10 rounded-full"
+							use:popup={popupHover}
+						/>
+					</a>
+					<div class="card p-4 shadow" data-popup="popupHover">
+						<div>{character.user_name}</div>
+					</div>
+				</li>
+				<li class="crumb-separator" aria-hidden>&rsaquo;</li>
+				<li class="crumb">
+					<a href="/elements/breadcrumbs" class="variant-soft-primary btn btn-sm">Characters</a>
+				</li>
+				<li class="crumb-separator" aria-hidden>&rsaquo;</li>
+				<li class="crumb">
+					{character.name}
+				</li>
+			</ol>
+		</svelte:fragment>
+
+		<svelte:fragment slot="sidebarLeft">
+			<h1>{character.name}</h1>
+			<h2>{character.hero}</h2>
+			<p>{character.gold} gold</p>
+			{character.level}
+			<ul>
+				{#each Object.keys(character.attributes) as attribute}
+					<li>
+						<strong>{attribute}:</strong>
+						{character.attributes[attribute]}
+					</li>
+				{/each}
+			</ul>
+			<ul>
+				{#each Object.keys(character.resistances) as resistance}
+					<li>
+						<strong>{resistance}:</strong>
+						{character.resistances[resistance]}
+					</li>
+				{/each}
+			</ul>
+		</svelte:fragment>
+
+		{#if items.length > 0}
+			<div class="grid grid-cols-3 gap-6">
+				{#each items as item}
+					<div class="variant-soft-surface rounded p-6 text-center">
+						<img
+							class="mx-auto h-20 object-contain"
+							src={item.imageUrl}
+							alt={item.name}
+							width="32"
+							height="32"
+						/>
+
+						<div class="font-bold" class:text-yellow-500={item.quality === 'yellow'}>
+							{item.name}
+						</div>
+						<div>{item.base_name}</div>
+						<div>{item.quality}</div>
+						<ul>
+							{#each item.properties.split(',') as property}
+								{#if property.trim()}
+									<li>{property}</li>
+								{/if}
+							{/each}
+						</ul>
+					</div>
+				{/each}
+			</div>
+		{:else}
+			<p>No items found.</p>
+		{/if}
+
+		<svelte:fragment slot="footer">
+			Updated {character.update_time}
+		</svelte:fragment>
+	</CharacterLayout>
+{/if}
